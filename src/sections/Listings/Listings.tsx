08382fb20bca16a1e3abfa908257694e7ad98ba0
supernova0606 <drivendev66@gmail.com>
2020-03-15 13:40:30 +0100

    Client gets listings by server GraphQL api query to MongoDB Atlas

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,20 +1,36 @@
 import React from "react";
+import { server } from "../../lib/api";
 
+const LISTINGS = `
+  query {
+    listings {
+      id,
+      title,
+      image,
+      address,
+      numberOfBedrooms,
+      numberOfBathrooms,
+      rating
+    }
+  }
+`;
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-  const fetchListings = () => {
-    console.log("click");
+  const fetchListings = async () => {
+    const { data } = await server.fetch({ query: LISTINGS });
+    console.log(data);
   };
+
   return (
     <main>
       <div>
         <h1>{title}</h1>
       </div>
       <div>
-        <button onClick={fetchListings}>Click me</button>
+        <button onClick={fetchListings}>Get Listings</button>
       </div>
     </main>
   );
